90e4e827c5753767112f9024c67c0cbbaa849660
mokshaadh <mokshaadh07@gmail.com>
2025-05-26 17:17:24 -0400

    first commit!

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use std::io::Write;
+
+fn main() {
+    loop {
+        let mut input = String::new();
+        print!("~=:> ");
+        std::io::stdout().flush().unwrap();
+        std::io::stdin().read_line(&mut input).unwrap();
+    }
+}
